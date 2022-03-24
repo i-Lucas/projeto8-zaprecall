a806eecf63533bb73d71963e3b08f8bf8f30a401
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-03-24 16:49:22 -0300

    Preparando revisão de código

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './Components/App/'
-
-// stylesheet`s
-import './assets/css/reset.css'
-import './assets/css/style.css'
-
-ReactDOM.render(<App />, document.querySelector('.root'))
